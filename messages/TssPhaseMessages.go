e32c498c78cd4a96cb8827a85db996b105fdfdc3
Norse Code <dev@thorchain.org>
2020-06-16 17:53:27 +0100

    gg20 and protobuf version bump

--- messages/TssPhaseMessages.go
+++ messages/TssPhaseMessages.go
@@ -1,9 +1,9 @@
 package messages
 
 const (
-	KEYGEN2aUnicast = "KGRound2Message1"
+	KEYGEN2aUnicast = "binance.tss-lib.ecdsa-gg20.keygen.KGRound2Message1"
 
-	KEYSIGN1aUnicast = "SignRound1Message1"
-	KEYSIGN1b        = "SignRound1Message2"
-	KEYSIGN2Unicast  = "SignRound2Message"
+	KEYSIGN1aUnicast = "binance.tss-lib.ecdsa-gg20.sign.SRound1Message1"
+	KEYSIGN1b        = "binance.tss-lib.ecdsa-gg20.sign.SRound1Message2"
+	KEYSIGN2Unicast  = "binance.tss-lib.ecdsa-gg20.sign.SRound2Message"
 )
