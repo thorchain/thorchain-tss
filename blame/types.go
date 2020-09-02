dc39a0754ac8ee0a3a97d7c732dbd2229cb17f44
Heimdall <heimdallthor02@gmail.com>
2020-09-02 11:34:03 +1000

    add a comment

--- blame/types.go
+++ blame/types.go
@@ -36,6 +36,7 @@ type Node struct {
 }
 
 // Blame is used to store the blame nodes and the fail reason
+// *** Blame struct had been referenced and registered in thornode , so please don't change this structure, otherwise it will have consensus failure when trying to update thornode  ***
 type Blame struct {
 	FailReason string `json:"fail_reason"`
 	IsUnicast  bool   `json:"is_broadcast"`
